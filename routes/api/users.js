a19d4ff98143c811780707922ef9ba2c44f7e028
m081779 <marcoprincipio@hotmail.com>
2018-02-02 11:28:19 -0500

    pushing non working passport code.  Users can signup, login, and logout, but cookies aren't being deleted reliably.  Sessions are being deleted though.

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,27 +5,28 @@ const passport = require('passport')
 //=============================================================================
 // AUTHENTICATION ROUTES =======================================================
 // =============================================================================
+  //process the login form
+  router.post('/login', passport.authenticate('local-login'), function (req,res){
+    let authObj = {user: req.user, isAuth: req.isAuthenticated()};
+    return res.json(authObj);
+  });
 
-  router.post('/login', passport.authenticate('local-login', {
-      successRedirect : '/dashboard', // redirect to the secure user dashboard section
-      failureRedirect : '/login', // redirect back to the login page if there is an error
-      failureFlash : true // allow flash messages
-  }));
   // process the signup form
-  router.post('/signup', passport.authenticate('local-signup', {
-      successRedirect : '/profile', // redirect to the profile create section
-      failureRedirect : '/signup', // redirect back to the signup page if there is an error
-      failureFlash : true // allow flash messages
-  }));
+  router.post('/signup', passport.authenticate('local-signup'), function (req,res) {
+    let authObj = {user: req.user, isAuth: req.isAuthenticated()};
+    return res.json(authObj);
+  });
 
-  // LOGOUT ==============================
+  // process the logout request
   router.get('/logout', function(req, res) {
+    console.log('9999999999999999999999999:', req.user)
     req.logout();
     req.session.destroy(function (err) {
       if (!err) {
-          res.status(200).clearCookie('connect.sid', {path: '/'}).redirect('/');
+        res.sendStatus(200).clearCookie('connect.sid', {path: '/'});
+        console.log('no error')
       } else {
-          console.log('Error from session destroy:', err)
+        console.log('Error from session destroy:', err)
       }
     });
   });
