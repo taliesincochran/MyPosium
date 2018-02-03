7be9c1aac79c8eae0394c85cb6d1bed5c0de8f8e
OtherSuccesses <viskaslietuvai@gmail.com>
2018-02-03 09:38:31 -0500

    Initial Routes setup

---
+++ client/src/pages/CreateEvent/CreateEvent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Container, Field, Label, Control, Input, Button } from 'bloomer';
+import { Link } from "react-router-dom";
+import axios from 'axios';
+
+export default class CreateEvent extends Component {
+  state = {
+
+  }
---
+++ client/src/pages/Event/Event.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Container, Field, Label, Control, Input, Button } from 'bloomer';
+import { Link } from "react-router-dom";
+import axios from 'axios';
+
+export default class Event extends Component {
+  state = {
+  }
