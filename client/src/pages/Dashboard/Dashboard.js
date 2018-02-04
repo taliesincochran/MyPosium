8761c211365758f77e0271a8259b1c1d49285ad6
m081779 <marcoprincipio@hotmail.com>
2018-02-04 01:50:29 -0500

    fixed redirects for login, signup, and logout

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,35 +1,30 @@
 import React, { Component } from 'react';
 import { Container, Button } from 'bloomer';
-// import { Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 
 export default class Dashboard extends Component {
   state = {
-
+    logout: false
   }
-//probably no longer needed since I think I solved the redirect from the backend
-//for logout.  But, keeping just in case
 
-  // handleLogout = () => {
-  //   axios
-  //     .get('api/users/logout')
-  //     .then(response => {
-  //       console.log('firing',response)
-  //       console.log(response.status)
-  //       if (response.status === 200){
-  //         console.log(this.props.location.state)
-  //       return  <Redirect to="/" />
-  //         // window.location.href = '/';
-  //       }
-  //     })
-  //     .catch(err => console.log(err));
-  // }
+  handleLogout = () => {
+    axios
+      .get('api/users/logout')
+      .then(response => {
+        if (response.status === 200){
+          this.setState({logout:true});
+        }
+      })
+      .catch(err => console.log(err));
+  }
 
   render() {
     return(
       <Container>
         <h1>Dashboard</h1>
         <Button onClick={this.handleLogout}>Logout</Button>
+        {this.state.logout? (<Redirect to="/" />) : null}
       </Container>
     )
   }
