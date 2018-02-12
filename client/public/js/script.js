5ecfb8cf8cccc3d8b79b69e4a0388ea5136d469a
m081779 <marcoprincipio@hotmail.com>
2018-02-11 21:22:25 -0500

    pushing new build

--- client/public/js/script.js
+++ client/public/js/script.js
@@ -1,10 +1,7 @@
 
 $(document).ready(function () {
-  $(window).scroll(function (){
-    console.log('firing');
-    let wScroll = $(this).scrollTop();
-    console.log(wScroll);
-  });
+
+
   $("a").click(function (event) {
     if (this.hash!=="") {
       event.preventDefault();
@@ -15,35 +12,12 @@ $(document).ready(function () {
         window.location.hash = hash;
       });
     }
+  });//end soft scroll function
 
-    $(window).scroll(function () {
-          var wScroll = $(this).scrollTop();
-          console.log(wScroll)
-          // end of scroll
-      });
-
-
-  });
+  $(window).scroll(function () {
+    var wScroll = $(this).scrollTop();
+    // console.log(wScroll)
+  });//end scroll event
 
 
-  if (window.location.pathname==='/dashboard'){
-    $('html, body').css({
-      'background-image': 'url("")',
-      'background-attachment': 'fixed'
-      //body.style.background = 'background: linear-gradient(to right, rgb(200,245,240), MintCream, MintCream, white, white, MintCream, MintCream, rgb(200,245,240));'
-    })
-  }
-  // if (window.location.pathname==='/signup' || window.location.pathname==='/login'){
-  //   $('html, body').css({
-  //     'background-image': 'url("../img/coloredLines.jpg")',
-  //     'background-attachment': 'fixed'
-  //   })
-  // }
-  if (window.location.pathname==='/'){
-    $('html, body').css({
-      'background-image': 'url("../img/woodBackground.jpg")',
-      'background-attachment': 'fixed',
-      'background-size': '100% 100%'
-    })
-  }
-});
+});//end document ready function
