73ee3613707a82e99bffddfc10e91181be8727bb
m081779 <marcoprincipio@hotmail.com>
2019-10-25 23:39:05 -0400

    fixed event zipcode lookup, corrected many small errors

--- routes/api/location.js
+++ routes/api/location.js
@@ -3,15 +3,14 @@ const axios = require('axios');
 //================================================================
 //fix the destination and origin issue by making get route a post.
 //================================================================
-router.get('/zipcode/:zipcode', async function(req,res) {
+router.get('/zipcode/:zipcode', function(req,res) {
 	const queryUrl = `https://www.zipcodeapi.com/rest/vnI7ZbrLcsJ1kwwkXWeBOM2dkWu0Fn63eNKWAwWAVYuvypn5p20NeIaiIHLeQntK/info.json/${req.params.zipcode}/degrees`;
-	const result = await axios.get(queryUrl);
-	res.json(result.data);
-})
-router.get('/destinations/:destinations', function(req,res) {
-	console.log(req.params.destinations);
-	axios.post(`https://maps.googleapis.com/maps/api/distancematrix/json?${req.params.destinations}&key=AIzaSyCEBI9Lm5hy3v-Dx18hokmV6_fXbVbXEm0`).then(result=>{
-		res.json(result.data)
-	})
-})
+	axios.get(queryUrl).then(result => res.json(result.data));
+});
+
+router.post('/destinations', function(req,res) {
+	const { destinations, zip } = req.body;
+	const queryUrl = `https://www.zipcodeapi.com/rest/vnI7ZbrLcsJ1kwwkXWeBOM2dkWu0Fn63eNKWAwWAVYuvypn5p20NeIaiIHLeQntK/multi-distance.json/${zip}/${destinations}/mile`;
+	axios.get(queryUrl).then(result=> res.json(result.data));
+});
 module.exports = router;
